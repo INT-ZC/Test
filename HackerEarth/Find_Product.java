04d19430a4e4bf9d493f7bb52d7f1b8296103c80
Zhiyuan  Chen <zc@int.ac.cn>
2017-10-23 05:37:46 +1100

    Commit

---
+++ HackerEarth/Find_Product.java
@@ -0,0 +1,24 @@
+package HackerEarth;
+
+import java.util.Scanner;
+
+public class Find_Product
+{
+	public static void main(String[] args) throws Exception
+	{
+		Scanner data = new Scanner(System.in);
+		int N = data.nextInt();
+		
+		long[] A = new long[N];
+		long p = 1000000007;
+		
+		long product = 1;
+		
+		for (int i = 0; i < N; i++)
+		{
+			A[i] = data.nextLong();
+			product = (product * A[i]) % p;
+		}
+		System.out.println(product);
+	}
+}
---
+++ HackerEarth/Palindromic_String.java
@@ -0,0 +1,14 @@
+package HackerEarth;
+
+import java.util.Scanner;
+
+public class Palindromic_String
+{
+	public static void main(String[] args) throws Exception
+	{
+		Scanner data = new Scanner(System.in);
+		String S = data.nextLine();
+		String s = new StringBuffer(S).reverse().toString();
+		System.out.println(S.equals(s)?"YES":"NO");
+	}
+}
